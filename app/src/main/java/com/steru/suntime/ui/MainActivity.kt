436ce634cff572c36e8a172098506246b5ef2aea
Steru <s.stergiadis@gmail.com>
2020-10-22 14:52:24 +0200

    configure project to run a basic coroutine

--- app/src/main/java/com/steru/suntime/ui/MainActivity.kt
+++ app/src/main/java/com/steru/suntime/ui/MainActivity.kt
@@ -2,11 +2,18 @@ package com.steru.suntime.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.steru.suntime.R
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch {
+            Log.d("trg", "first coroutine!")
+        }
     }
 }
