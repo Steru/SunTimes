94927114ef1ade0b81b899b39a8d45a8976a7553
Steru <s.stergiadis@gmail.com>
2020-10-25 10:50:25 +0100

    test the time formatter
    the date and second count is tested properly,
    but formatting to local value is still a todo.
    we only check if it returns a null (if it's a bad parse)
    or not null (if it's parsed correctly)

--- app/src/main/java/com/steru/suntime/ui/utils/TimeFormatter.kt
+++ app/src/main/java/com/steru/suntime/ui/utils/TimeFormatter.kt
@@ -1,9 +1,12 @@
 package com.steru.suntime.ui.utils
 
 import android.util.Log
-import java.text.ParseException
+import java.lang.NumberFormatException
+import java.time.DateTimeException
+import java.time.LocalTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
 
 /**
  * Class used to format time strings.
@@ -18,10 +21,7 @@ class TimeFormatter {
         .ofPattern("yyyy-MM-dd'T'HH:mm:ssZZZZZ")
         .withZone(ZoneId.of("UTC"))
 
-    private val secondFormat = DateTimeFormatter.ofPattern("ss")
-
     val dateFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
-
 
     /**
      * Format date string from UTC full date to 24-hour localised format
@@ -31,7 +31,7 @@ class TimeFormatter {
             this.fullDateFormatUTC.parse(fullDate)?.let {
                 return hoursMinutesFormatLocal.format(it)
             }
-        } catch (e: ParseException) {
+        } catch (e: DateTimeParseException) {
             handleParseException(e)
         }
 
@@ -39,15 +39,19 @@ class TimeFormatter {
     }
 
     /**
-     * Format seconds string to 24-hour format
+     * Format seconds-of-day string to 24-hour format
+     *
+     * Allows values up to (24 * 3600) - 1
      */
-    fun formatSecondsToHours(date: String): String? {
+    fun formatSecondCountToHours(secondCount: String): String? {
         try {
-            secondFormat.parse(date)?.let {
-                return hoursMinutesFormatLocal.format(it)
+            return hoursMinutesFormatLocal.format(getTimeFromSecondCount(secondCount))
+        } catch (e: Exception) {
+            when (e) {
+                is DateTimeException,
+                is NumberFormatException -> handleParseException(e)
+                else -> throw e
             }
-        } catch (e: ParseException) {
-            handleParseException(e)
         }
 
         return null
@@ -59,13 +63,17 @@ class TimeFormatter {
     fun formatFullDateTimeToDate(date: String): String? {
         try {
             return dateFormat.format(fullDateFormatUTC.parse(date))
-        } catch (e: ParseException) {
+        } catch (e: DateTimeParseException) {
             handleParseException(e)
         }
         return null
     }
 
-    private fun handleParseException(e: ParseException) {
+    private fun getTimeFromSecondCount(secondCount: String): LocalTime? {
+        return LocalTime.ofSecondOfDay(Integer.parseInt(secondCount).toLong())
+    }
+
+    private fun handleParseException(e: Exception) {
         //todo there may be a need of returning some error state
         // but for now, just a log and null string will do
         Log.e("TimeFormatter", "Parse exception: ${e.message}")
