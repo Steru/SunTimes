10ea023a707115bc4f21278cafbd7e9bc083d5d1
Steru <s.stergiadis@gmail.com>
2020-10-26 17:25:20 +0100

    fix time format having to timeZone ifo
    after switching to java.time, something got mixed up with zones. now it should be ok.

--- app/src/main/java/com/steru/suntime/ui/utils/TimeFormatter.kt
+++ app/src/main/java/com/steru/suntime/ui/utils/TimeFormatter.kt
@@ -2,27 +2,34 @@ package com.steru.suntime.ui.utils
 
 import android.util.Log
 import java.text.ParseException
+import java.time.ZoneId
 import java.time.format.DateTimeFormatter
-import java.util.*
 
 /**
  * Class used to format time strings.
  */
 class TimeFormatter {
 
-    private val hoursMinutesFormat = DateTimeFormatter.ofPattern("HH:mm", Locale.getDefault())
-    private val fullDateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZZZZZ", Locale.getDefault())
-    private val secondFormat = DateTimeFormatter.ofPattern("ss", Locale.getDefault())
-    val dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+    private val hoursMinutesFormatLocal = DateTimeFormatter
+        .ofPattern("HH:mm")
+        .withZone(ZoneId.systemDefault())
+
+    private val fullDateFormatUTC = DateTimeFormatter
+        .ofPattern("yyyy-MM-dd'T'HH:mm:ssZZZZZ")
+        .withZone(ZoneId.of("UTC"))
+
+    private val secondFormat = DateTimeFormatter.ofPattern("ss")
+
+    val dateFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
 
 
     /**
-     * Format date string from full date to 24-hour localised format
+     * Format date string from UTC full date to 24-hour localised format
      */
     fun formatTo24hLocalTime(fullDate: String): String? {
         try {
-            this.fullDateFormat.parse(fullDate)?.let {
-                return hoursMinutesFormat.format(it)
+            this.fullDateFormatUTC.parse(fullDate)?.let {
+                return hoursMinutesFormatLocal.format(it)
             }
         } catch (e: ParseException) {
             handleParseException(e)
@@ -37,7 +44,7 @@ class TimeFormatter {
     fun formatSecondsToHours(date: String): String? {
         try {
             secondFormat.parse(date)?.let {
-                return hoursMinutesFormat.format(it)
+                return hoursMinutesFormatLocal.format(it)
             }
         } catch (e: ParseException) {
             handleParseException(e)
@@ -51,7 +58,7 @@ class TimeFormatter {
      */
     fun formatFullDateTimeToDate(date: String): String? {
         try {
-            return dateFormat.format(fullDateFormat.parse(date))
+            return dateFormat.format(fullDateFormatUTC.parse(date))
         } catch (e: ParseException) {
             handleParseException(e)
         }
