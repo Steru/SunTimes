9134e45ff2bb4fc34c0e9c6fa5b821bd8460c26b
Steru <s.stergiadis@gmail.com>
2020-10-26 11:53:07 +0100

    use java.time for time handling
    some build.gradle changes were needed, (recommended by Jake Wharton!)
    desugaring allows to use the java 8 features below api 26

--- app/src/main/java/com/steru/suntime/ui/utils/TimeFormatter.kt
+++ app/src/main/java/com/steru/suntime/ui/utils/TimeFormatter.kt
@@ -2,21 +2,26 @@ package com.steru.suntime.ui.utils
 
 import android.util.Log
 import java.text.ParseException
-import java.text.SimpleDateFormat
+import java.time.format.DateTimeFormatter
 import java.util.*
 
+/**
+ * Class used to format time strings.
+ */
 class TimeFormatter {
 
-    private val hoursMinutesFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
-    private val amPmFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.getDefault())
-    private val secondFormat = SimpleDateFormat("ss", Locale.getDefault())
+    private val hoursMinutesFormat = DateTimeFormatter.ofPattern("HH:mm", Locale.getDefault())
+    private val fullDateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZZZZZ", Locale.getDefault())
+    private val secondFormat = DateTimeFormatter.ofPattern("ss", Locale.getDefault())
+    val dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+
 
     /**
      * Format date string from full date to 24-hour localised format
      */
     fun formatTo24hLocalTime(fullDate: String): String? {
         try {
-            amPmFormat.parse(fullDate)?.let {
+            this.fullDateFormat.parse(fullDate)?.let {
                 return hoursMinutesFormat.format(it)
             }
         } catch (e: ParseException) {
@@ -41,6 +46,17 @@ class TimeFormatter {
         return null
     }
 
+    /**
+     * Format full date / time string to just date
+     */
+    fun formatFullDateTimeToDate(date: String): String? {
+        try {
+            return dateFormat.format(fullDateFormat.parse(date))
+        } catch (e: ParseException) {
+            handleParseException(e)
+        }
+        return null
+    }
 
     private fun handleParseException(e: ParseException) {
         //todo there may be a need of returning some error state
